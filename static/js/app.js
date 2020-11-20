3189e62d68a14946d2c5ecb3786f54f03d2972f8
ipavlunin <ipavlunin@gmail.com>
2020-11-19 21:46:16 -0600

    variables added

--- static/js/app.js
+++ static/js/app.js
@@ -4,5 +4,18 @@ d3.selectAll("#sample-metadata").select("h6").remove();
 d3.json("samples.json").then(function (data) {
     console.log(data);
 
+    // Creating Variables
+    var names = data.names
+    console.log(names)
+    var metadata = data.metadata
+    console.log(metadata)
+    var samples = data.samples
+    console.log(samples)
+    var samplevalues = samples.map(data => data.sample_values)
+    console.log(samplevalues)
+    var otuids = samples.map(data => data.otu_ids)
+    console.log(otuids)
+    var otulabels = samples.map(data => data.otu_labels)
+    console.log(otulabels)
 
 })
